7bf16fa354981d15652d3d1803d70428e237bb23
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-12-09 16:58:08 -0600

    WIP

--- app/assets/javascripts/admin/datatables.js
+++ app/assets/javascripts/admin/datatables.js
@@ -1,34 +1,21 @@
-$(document).on('turbolinks:load', function () {
-  const tableSelectors = [
-    '#purpose-travel-patterns-table',
-    '#funding-sources-table',
-    '#booking-profiles-table',
-    '#DataTables_Table_0_wrapper',
-  ];
+$(document).on('turbolinks:load', function() {
+  const tableSelectors = '#purpose-travel-patterns-table, #funding-sources-table, #booking-profiles-table, #DataTables_Table_0_wrapper; #DataTables_Table_0';
 
-  tableSelectors.forEach((selector) => {
-    if ($.fn.DataTable.isDataTable(selector)) {
-      $(selector).dataTable().fnDestroy();
-    }
+  if ($.fn.DataTable.isDataTable(tableSelectors)) {
+    $(tableSelectors).DataTable().destroy();
+  }
 
-    $(selector).dataTable({
-      columnDefs: [
-        {
-          targets: 2,
-          orderable: false,
-        },
-      ],
-      lengthMenu: [10, 25, 50],
-      autoWidth: false,
-      destroy: true,
-    });
+  let dataTable = $(tableSelectors).DataTable({
+    "columnDefs": [ {
+      "targets": 2,
+      "orderable": false
+    } ]
   });
 
-  document.addEventListener('turbolinks:before-cache', function () {
-    tableSelectors.forEach((selector) => {
-      if ($.fn.DataTable.isDataTable(selector)) {
-        $(selector).dataTable().fnDestroy();
-      }
-    });
+  document.addEventListener("turbolinks:before-cache", function() {
+    if ($.fn.DataTable.isDataTable(tableSelectors)) {
+      dataTable.destroy();
+      dataTable = null;
+    }
   });
 });
