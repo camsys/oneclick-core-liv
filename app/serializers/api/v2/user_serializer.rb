52c6f4179588da78ecbec13f6f2b74468b07b1b6
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-12-17 14:07:42 -0600

    WIP

--- app/serializers/api/v2/user_serializer.rb
+++ app/serializers/api/v2/user_serializer.rb
@@ -1,37 +1,29 @@
 module Api
   module V2
-
     class UserSerializer < ApiSerializer
-      
-      attributes  :first_name, 
-                  :last_name, 
-                  :email,
-                  :preferred_locale,
-                  :trip_types,
-                  :age,
-                  :county,
-                  :paratransit_id,
-                  :counties
-      
+      attributes :id, :first_name, :last_name, :email, :preferred_locale,
+                 :trip_types, :age, :county, :paratransit_id
+
       has_many :eligibilities
       has_many :accommodations
-      has_many :trip_types
-      has_many :counties
-    
+
+      def counties
+        County.all.map { |county| { name: county.name } }
+      end
+
+      def preferred_locale
+        object.locale&.name
+      end
+
       def trip_types
         Trip::TRIP_TYPES.map do |trip_type|
           {
             code: trip_type,
             name: SimpleTranslationEngine.translate(locale, "mode_#{trip_type}_name"),
-            value: (trip_type.to_s.in? (object.preferred_trip_types || []))
+            value: object.preferred_trip_types&.include?(trip_type.to_s)
           }
         end
-        
-      def preferred_locale
-        object.locale.try(:name)
       end
-
     end
-    
   end
 end
