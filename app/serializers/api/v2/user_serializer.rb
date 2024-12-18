5cb79f01fbfc3c93901c220fd34f47a14b3e6c6f
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2024-12-18 11:20:28 -0600

    WIP

--- app/serializers/api/v2/user_serializer.rb
+++ app/serializers/api/v2/user_serializer.rb
@@ -1,31 +1,52 @@
 module Api
   module V2
-    class UserSerializer < ApiSerializer
-      attributes :id, :first_name, :last_name, :email, :preferred_locale,
-                 :trip_types, :age, :county, :paratransit_id
 
+    class UserSerializer < ApiSerializer
+      
+      attributes  :first_name, 
+                  :last_name, 
+                  :email,
+                  :preferred_locale,
+                  :trip_types,
+                  :age,
+                  :county,
+                  :paratransit_id,
+                  :counties
+      
       has_many :eligibilities
       has_many :accommodations
-      has_many :counties
       
+      def eligibilities
+        scope[:user] ||= object # set user in scope
+        Eligibility.all
+      end
+      
+      def accommodations
+        scope[:user] ||= object # set user in scope
+        Accommodation.all
+      end
 
       def counties
+        scope[:user] ||= object # set user in scope
         County.all.map { |county| { name: county.name } }
       end
 
-      def preferred_locale
-        object.locale&.name
+      def trip_types
+        Trip::TRIP_TYPES.map {
+          |trip_type| 
+            { 
+              code: trip_type,
+              name: SimpleTranslationEngine.translate(locale, "mode_#{trip_type}_name"),
+              value: (trip_type.to_s.in? (object.preferred_trip_types || []))
+            }
+        }
       end
 
-      def trip_types
-        Trip::TRIP_TYPES.map do |trip_type|
-          {
-            code: trip_type,
-            name: SimpleTranslationEngine.translate(locale, "mode_#{trip_type}_name"),
-            value: object.preferred_trip_types&.include?(trip_type.to_s)
-          }
-        end
+      def preferred_locale
+        object.locale.try(:name)
       end
+
     end
+    
   end
 end
