d9493a512cf2940177378f3372e10b2b04828b07
Jalen Sowell <jsowell@SOWELLJ-MAC.local>
2025-05-28 12:21:06 -0500

    Adding loggers to see if I can get the entitlements call to work on update

--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -16,28 +16,42 @@ module Api
       end
 
       # Update's the user's profile
+      # Update's the user's profile
       def update
         unless @traveler.present?
+          Rails.logger.warn "[UsersController#update] No traveler found for ID=#{params[:id]}"
           render(fail_response(status: 404, message: "Not found")) and return
         end
 
         old_age = @traveler.age.to_i
+        Rails.logger.debug "[UsersController#update] Current age for #{@traveler.email}: #{old_age}"
 
         begin
           if @traveler.update_profile(params)
             new_age = @traveler.age.to_i
+            Rails.logger.debug "[UsersController#update] Updated age for #{@traveler.email}: #{new_age}"
 
-            # if they just turned 65+, add the Senior entitlement
             if old_age < 65 && new_age >= 65
-              JustrideClient.add_senior_entitlement(@traveler.justride_account_id)
+              account_id = @traveler.justride_account_id
+              Rails.logger.info  "[UsersController#update] Age crossed 65 for #{@traveler.email}, adding Senior entitlement to Justride account #{account_id}"
+              begin
+                resp = JustrideClient.add_senior_entitlement(account_id)
+                Rails.logger.info "[UsersController#update] Justride add_senior_entitlement response: #{resp.inspect}"
+              rescue => e
+                Rails.logger.error "[UsersController#update] Failed to add Senior entitlement for account #{account_id}: #{e.class} #{e.message}"
+              end
+            else
+              Rails.logger.debug "[UsersController#update] No entitlement action needed (old_age=#{old_age}, new_age=#{new_age})"
             end
 
             set_locale  # based on traveler's new preferred locale
             render(success_response(@traveler))
           else
+            Rails.logger.warn "[UsersController#update] update_profile failed for #{@traveler.email}: #{ @traveler.errors.full_messages.join(', ') }"
             render(fail_response(status: 400, message: "Unable to update."))
           end
         rescue => exception
+          Rails.logger.error "[UsersController#update] Exception during update for #{@traveler.email}: #{exception.class} #{exception.message}\n#{exception.backtrace.first(5).join("\n")}"
           render(fail_response(status: 400, message: "Unable to update."))
         end
       end
